d3ba15218a5add00cc68b8e75579b28e75e5c618
João Mota <joaommota.work@gmail.com>
2022-03-09 12:06:35 +0000

    feat(setup): redux toolkit, date-fns, next-translate, editorJs

--- apps/musigrow/pages/_app.tsx
+++ apps/musigrow/pages/_app.tsx
@@ -1,18 +1,22 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import 'tailwindcss/tailwind.css';
+import { store } from '../app/store';
+import { Provider } from 'react-redux';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to musigrow!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+	return (
+		<Provider store={store}>
+			<>
+				<Head>
+					<title>Welcome to musigrow!</title>
+				</Head>
+				<main className='app'>
+					<Component {...pageProps} />
+				</main>
+			</>
+		</Provider>
+	);
 }
 
 export default CustomApp;
--- apps/musigrow/pages/index.tsx
+++ apps/musigrow/pages/index.tsx
@@ -1,18 +1,46 @@
-export function Index() {
+import React from 'react';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import { decrement, increment } from '../features/counter/counterSlice';
+import { useGetPokemonByNameQuery } from '../services/pokemon';
+import useTranslation from 'next-translate/useTranslation';
+
+export function Counter() {
+	const { t } = useTranslation();
+
+	// The `state` arg is correctly typed as `RootState` already
+	const count = useAppSelector((state) => state.counter.value);
+	const dispatch = useAppDispatch();
+
+	// Using a query hook automatically fetches data and returns query values
+	const { data, error, isLoading } = useGetPokemonByNameQuery('pikachu');
+
 	return (
-		<article className='prose lg:prose-xl'>
-			<h1>Garlic bread with cheese: What the science tells us</h1>
-			<p>
-				For years parents have espoused the health benefits of eating garlic bread with cheese to their
-				children, with the food earning such an iconic status in our culture that kids will often dress up as
-				warm, cheesy loaf for Halloween.
-			</p>
-			<p>
-				But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-				springing up around the country.
-			</p>
-		</article>
+		<div>
+			<div>{t('hello')}</div>
+			<div>{t('variableExample', { count: 42 })}</div>
+			<div>
+				<button aria-label='Increment value' onClick={() => dispatch(increment())}>
+					Increment
+				</button>
+				<span>{count}</span>
+				<button aria-label='Decrement value' onClick={() => dispatch(decrement())}>
+					Decrement
+				</button>
+			</div>
+			<div className='App'>
+				{error ? (
+					<>Oh no, there was an error</>
+				) : isLoading ? (
+					<>Loading...</>
+				) : data ? (
+					<>
+						<h3>{data.species.name}</h3>
+						<img src={data.sprites.front_shiny} alt={data.species.name} />
+					</>
+				) : null}
+			</div>
+		</div>
 	);
 }
 
-export default Index;
+export default Counter;
