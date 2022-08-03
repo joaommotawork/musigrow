f247995abe4fc619e3db5e23a5f01115f239ae73
João Mota <joaommota.work@gmail.com>
2022-08-03 23:56:29 +0100

    feat(setupRepo): shared configs

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: [
+		// App Content
+		`src/**/*.{js,ts,jsx,tsx}`,
+		// Include Packages If Not Transpiling
+		// "../../packages/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
