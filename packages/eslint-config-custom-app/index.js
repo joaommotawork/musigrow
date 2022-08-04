dc9beea5856a0a291fa4e948fe63b76ffb123a8a
João Mota <joaommota.work@gmail.com>
2022-08-05 00:53:51 +0100

    feat(setupRepo): eslint config custom app

--- packages/eslint-config-custom-app/index.js
+++ packages/eslint-config-custom-app/index.js
@@ -1,6 +1,42 @@
 module.exports = {
-	extends: ['next', 'prettier'],
+	env: {
+		'browser': true,
+		'es2021': true,
+		'jest/globals': true,
+	},
+	extends: [
+		'eslint:recommended',
+		'@infinumjs/eslint-config-nextjs-ts',
+		'airbnb',
+		'airbnb/hooks',
+		'next',
+		'prettier',
+	],
 	rules: {},
+	overrides: [
+		{
+			files: [
+				'**/__tests__/**/*.[jt]s?(x)',
+				'**/?(*.)+(spec|test).[jt]s?(x)',
+			],
+			plugins: ['testing-library', 'jest'],
+			extends: ['plugin:jest/all', 'plugin:testing-library/react'],
+		},
+		{
+			files: ['*.ts', '*.tsx'],
+			parserOptions: {
+				project: ['../tsconfig/musigrow-app.json'],
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 	settings: {
 		react: {
 			version: 'detect',
