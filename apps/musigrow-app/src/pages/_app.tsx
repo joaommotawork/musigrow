63d3fc4f87e7650436aec6fabf015637d5b0a77a
João Mota <joaommota.work@gmail.com>
2022-08-05 01:03:24 +0100

    feat(setupAppUI): axe core

--- apps/musigrow-app/src/pages/_app.tsx
+++ apps/musigrow-app/src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axe from '@axe-core/react';
+
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+	axe(React, ReactDOM, 1000);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
--- packages/musigrow-ui/.storybook/preview.js
+++ packages/musigrow-ui/.storybook/preview.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@styles/main.css';
 
+if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
+	const axe = require('@axe-core/react');
+	axe(React, ReactDOM, 1000);
+}
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	layout: 'centered',
