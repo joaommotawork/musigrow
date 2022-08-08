b730fde825c16e4b9ac0b3148f9151edd2b0bc28
João Mota <joaommota.work@gmail.com>
2022-08-09 00:03:26 +0100

    feat(setupApp): nextjs google analytics

--- apps/musigrow-app/src/pages/_app.tsx
+++ apps/musigrow-app/src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { appWithTranslation } from 'next-i18next';
+import { GoogleAnalytics, usePageViews, event } from 'nextjs-google-analytics';
 import { setupStore } from '@app/store';
 import Layout from '@components/Layout/Layout';
 import '@vime/core/themes/default.css';
@@ -13,13 +14,33 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
 	axe(React, ReactDOM, 1000);
 }
 
+export function reportWebVitals({
+	id,
+	name,
+	label,
+	value,
+}: NextWebVitalsMetric) {
+	event(name, {
+		category:
+			label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+		value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+		label: id, // id unique to current page load
+		nonInteraction: true, // avoids affecting bounce rate.
+	});
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+	usePageViews();
+
 	return (
-		<Provider store={setupStore()}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</Provider>
+		<>
+			<GoogleAnalytics />
+			<Provider store={setupStore()}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</Provider>
+		</>
 	);
 }
 
