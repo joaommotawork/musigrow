d140e2b66adc6b4c39a2eb540204db35f0d2b8bf
João Mota <joaommota.work@gmail.com>
2022-08-08 22:29:24 +0100

    fix(setupApp): remove missing types

--- apps/musigrow-app/src/services/pokemon.ts
+++ apps/musigrow-app/src/services/pokemon.ts
@@ -1,13 +1,12 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { Pokemon } from './types';
 
 // Define a service using a base URL and expected endpoints
 export const pokemonApi = createApi({
 	reducerPath: 'pokemonApi',
 	baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
 	endpoints: (builder) => ({
-		getPokemonByName: builder.query<Pokemon, string>({
+		getPokemonByName: builder.query<any, string>({
 			query: (name) => `pokemon/${name}`,
 		}),
 	}),
