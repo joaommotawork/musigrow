b45a67085a8ce37e1d79cc1167db59c8a7485277
João Mota <joaommota.work@gmail.com>
2022-08-11 14:51:45 +0100

    fix(setupApi): security vulnerability

--- apps/musigrow-api/src/app.ts
+++ apps/musigrow-api/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { hidePoweredBy } from 'helmet';
 
 import exampleRouter from '@routes/examples/examples.router';
 
 const app = express();
 
+app.use(hidePoweredBy());
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(morgan('combined'));
 
