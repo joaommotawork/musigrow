ab6b2ad31c88b46d6d51ab240ff0b8094b442ab1
João Mota <joaommota.work@gmail.com>
2022-08-24 01:01:45 +0100

    feat(setupRepo): tailwind config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/typography'),
+		require('@tailwindcss/line-clamp'),
 		require('tailwindcss-debug-screens'),
 	],
 };
