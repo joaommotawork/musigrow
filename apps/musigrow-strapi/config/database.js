9694db312697bc22c8f67c22e71b62d65bdbb573
João Mota <joaommota.work@gmail.com>
2022-08-25 02:16:05 +0100

    feat(setupStrapi): deploy

--- apps/musigrow-strapi/config/database.js
+++ apps/musigrow-strapi/config/database.js
@@ -1,15 +1,19 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
-	connection: {
-		client: 'sqlite',
-		connection: {
-			filename: path.join(
-				__dirname,
-				'..',
-				env('DATABASE_FILENAME', '.tmp/data.db'),
-			),
+	defaultConnection: 'default',
+	connections: {
+		default: {
+			connector: 'bookshelf',
+			settings: {
+				client: 'postgres',
+				host: env('DATABASE_HOST', 'localhost'),
+				port: env.int('DATABASE_PORT', 5432),
+				database: env('DATABASE_NAME', 'strapi'),
+				username: env('DATABASE_USERNAME', 'strapi'),
+				password: env('DATABASE_PASSWORD', 'strapi'),
+				schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+				ssl: false,
+			},
+			options: {},
 		},
-		useNullAsDefault: true,
 	},
 });
--- apps/musigrow-strapi/config/env/production/database.js
+++ apps/musigrow-strapi/config/env/production/database.js
@@ -1,7 +1,7 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
 	connection: {
 		client: 'postgres',
 		connection: {
